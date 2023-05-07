f8b1234b1ebbd7d5e79d288fd912c49e92778d38
jessicarocham <jessicarocham18@hotmail.com>
2023-05-07 18:48:43 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+function reverseString(word) {
+  // create an array from the input string
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  return word === reversedWord;
 }
 
 /* 
-  Add your pseudocode here
+ create an array from the input string
+reverse the array
+create a string from the reversed array
+return the reversed string
 */
 
 /*
@@ -12,9 +23,28 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
